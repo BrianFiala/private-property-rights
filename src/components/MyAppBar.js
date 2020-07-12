f860cd084a415778bc8bc51e70033b30abeeb623
Brian Fiala <bgfiala@gmail.com>
2020-07-12 15:17:48 -0500

    added server side auth with google

--- src/components/MyAppBar.js
+++ src/components/MyAppBar.js
@@ -1,9 +1,13 @@
 import {h} from 'preact' /** @jsx h */
-import {useHeaderState} from '../contexts/HeaderStateProvider'
 import MenuIcon from '@material-ui/icons/Menu'
+import LockIcon from '@material-ui/icons/Lock'
+import CheckBoxIcon from '@material-ui/icons/CheckBox'
+import CheckBoxBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank'
 import BrightnessIcon from '@material-ui/icons/Brightness4Outlined'
 import {makeStyles, useTheme} from '@material-ui/core/styles'
 import {AppBar, IconButton, Slide, Toolbar, Typography, useScrollTrigger} from '@material-ui/core'
+import {useHeaderState} from '../contexts/HeaderStateProvider'
+import {useAdminState} from '../contexts/AdminStateProvider'
 
 const useStyles = makeStyles(theme => ({
   appBar: {
@@ -15,10 +19,14 @@ const useStyles = makeStyles(theme => ({
     [theme.breakpoints.up('sm')]: {
       marginLeft: theme.spacing(6)
     }
+  },
+  adminMode: {
+    marginLeft: theme.spacing(2)
   }
 }))
 
 export default function MyAppBar({toggleTheme}) {
+  const {userProfile, logout, toggleAdminMode, adminModeEnabled} = useAdminState()
   const {toggleDrawer} = useHeaderState()
   const classes = useStyles(useTheme())
   const trigger = useScrollTrigger({threshold: 32})
@@ -45,6 +53,32 @@ export default function MyAppBar({toggleTheme}) {
             className={classes.title}>
             Private Property Rights
           </Typography>
+          <div style={`${userProfile ? '' : 'display: none;'}`} class="g-signin2" data-onsuccess="onSignIn" data-theme="dark" />
+          {userProfile && (
+            <>
+              <Typography
+                component="h1"
+                variant="h6"
+                color="inherit"
+                noWrap
+                alignRight
+                displayInline
+                className={classes.adminMode}>
+                Admin Mode
+              </Typography>
+              <IconButton
+                edge="end"
+                color="inherit"
+                aria-label="toggleAdminMode"
+                onClick={toggleAdminMode}>
+                {adminModeEnabled ? (
+                  <CheckBoxIcon aria-hidden="true" />
+                ) : (
+                  <CheckBoxBlankIcon aria-hidden="true" />
+                )}
+              </IconButton>
+            </>
+          )}
           <IconButton
             edge="end"
             color="inherit"
